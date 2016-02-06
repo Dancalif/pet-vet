e98d079952b98483438549e075fa22cbb96df210
Dancalif <dancalif@gmail.com>
2016-02-06 15:47:48 -0800

    Adding Reptile class

---
+++ src/main/java/com/sqa/du/pet/vet/Reptile.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.sqa.du.pet.vet;
+
+/**
+ * @author dancalif
+ *
+ */
+public class Reptile extends Pet {
+	private boolean scales;
+	private int heatLevel;
+	private boolean poisonous;
+
+}
