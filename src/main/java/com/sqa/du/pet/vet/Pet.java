4204d36c51f5c11a1a7b63db7c3028869cf18344
Dancalif <dancalif@gmail.com>
2016-02-06 15:45:53 -0800

    Adding parent class Pet

---
+++ src/main/java/com/sqa/du/pet/vet/Pet.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.sqa.du.pet.vet;
+
+/**
+ * @author dancalif
+ *
+ */
+public class Pet {
+
+	private boolean alive;
+	private String name;
+	private boolean injured;
+	private boolean male;
+	private String breed;
+
+}
