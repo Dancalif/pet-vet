ae76c1ffc1c31cb96ca8a514a577c7531b80f154
Dancalif <dancalif@gmail.com>
2016-02-08 19:20:59 -0800

    Updating Snake class

--- src/main/java/com/sqa/du/pet/vet/Snake.java
+++ src/main/java/com/sqa/du/pet/vet/Snake.java
@@ -1,7 +1,47 @@
 package com.sqa.du.pet.vet;
 
+import com.sqa.du.util.helper.RequestInput;
+
 public class Snake extends Reptile {
 
 	private int shedFrequency;
+
+	/**
+	 * @param shedFrequency
+	 */
+	public Snake() {
+		super();
+		this.shedFrequency = RequestInput.getInt("How often does " + this.getName() + " shed of skin?");
+	}
+
+	/**
+	 * @param shedFrequency
+	 */
+	public Snake(int shedFrequency) {
+		super();
+		this.shedFrequency = shedFrequency;
+	}
+
+	public Snake(String name) {
+		this(RequestInput.getInt("How often does " + name + " shed of skin?"));
+	}
+
+	public int getShedFrequency() {
+		return shedFrequency;
+	}
+
+	public void setShedFrequency(int shedFrequency) {
+		this.shedFrequency = shedFrequency;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append(super.toString());
+		builder.append(", shedFrequency=");
+		builder.append(shedFrequency);
+		builder.append("]");
+		return builder.toString();
+	}
 
 }
